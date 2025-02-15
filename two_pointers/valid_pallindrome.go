1070c89664e0ccf2c5f316dedf83abf3bd7faff6
Nitin Misra <nitin.misra23@gmail.com>
2025-02-15 20:14:37 +0700

    feat: Add two pointers problem valid pallindrome

---
+++ two_pointers/valid_pallindrome.go
@@ -0,0 +1,51 @@
+package two_pointers
+
+import "strings"
+
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and
+removing all non-alphanumeric characters, it reads the same forward and backward.
+Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+	Input: s = "A man, a plan, a canal: Panama"
+	Output: true
+	Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+	Input: s = "race a car"
+	Output: false
+	Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+	Input: s = " "
+	Output: true
+	Explanation: s is an empty string "" after removing non-alphanumeric characters.
+	Since an empty string reads the same forward and backward, it is a palindrome.
+*/
+
+func IsPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	cleaned := ""
+	for _, char := range s {
+		if (char >= 'a' && char <= 'z') || (char >= '0' && char <= '9') {
+			cleaned += string(char)
+		}
+	}
+
+	left, right := 0, len(cleaned)-1
+	isPallindrome := true
+
+	for left <= right {
+		if cleaned[left] != cleaned[right] {
+			isPallindrome = false
+		}
+
+		left++
+		right--
+	}
+
+	return isPallindrome
+}
